cf04199711967056245d3ca371a0e157a8047d3d
Darko <dare.trajanov@gmail.com>
2024-10-29 01:35:49 +0100

    Final Update

--- src/pages/Chekiranje.jsx
+++ src/pages/Chekiranje.jsx
@@ -1,4 +1,4 @@
-import CheckinBooking from '../../features/check-in-out/CheckinBooking';
+import CheckinBooking from '../features/check-in-out/CheckinBooking';
 function Chekiranje() {
   return <CheckinBooking />;
 }
--- src/services/supabase.js
+++ src/services/supabase.js
@@ -2,7 +2,7 @@ import { createClient } from '@supabase/supabase-js';
 
 export const supabaseUrl = 'https://yxbmtapruqopmffvuact.supabase.co';
 const supabaseKey =
-  'eyJhbGcieyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4Ym10YXBydXFvcG1mZnZ1YWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDIxOTMsImV4cCI6MjA0NDMxODE5M30.5hLVYTwlojZkmfZp-CCBLoXLiKx2wPBpTkpidTuM5hU';
+  'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inl4Ym10YXBydXFvcG1mZnZ1YWN0Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3Mjg3NDIxOTMsImV4cCI6MjA0NDMxODE5M30.5hLVYTwlojZkmfZp-CCBLoXLiKx2wPBpTkpidTuM5hU';
 const supabase = createClient(supabaseUrl, supabaseKey);
 
 export default supabase;
